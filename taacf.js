54980df1f3ac116e7bbb17251c94f1ee2d066795
J2csummit <J2csummit@mac.com>
2014-08-13 13:38:31 -0400

    Third Commit
    Huge change to “Schedule”, added Javascript file to make things scroll
    while the header/footers remain static

---
+++ taacf.js
@@ -0,0 +1,12 @@
+function get_window_height(){
+    // var header = $('#headerpt1').outerHeight(true) + $('#headerpt2').outerHeight(true);
+    // var footer = $('#footer').outerHeight(true);
+    var winDow = $(window).height();
+    var height = (winDow - 35 - 79 - 36 + 5);
+    $(".ui-content").css('height', height);
+}
+function get_about_height(){
+	var winDow = $(window).height();
+    var height = (winDow - 79);
+    $(".ui-content").css('height', height);
+}
