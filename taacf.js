488a32ec9bae9ead843fa6cd22a94ba8febe1909
J2csummit <J2csummit@mac.com>
2014-08-13 14:04:19 -0400

    4
    Testing out resize function

--- taacf.js
+++ taacf.js
@@ -2,15 +2,20 @@ function get_window_height(){
     // var header = $('#headerpt1').outerHeight(true) + $('#headerpt2').outerHeight(true);
     // var footer = $('#footer').outerHeight(true);
     var winDow = $(window).height();
-    var height = (winDow - 35 - 79 - 36 + 5);
-    $(".ui-content").css('height', height);
+    var height = (winDow - 35 - 79 - 36 + 8);
+    $("#content_Schedule").css('height', height);
 }
 function get_about_height(){
 	var winDow = $(window).height();
     var height = (winDow - 79);
-    $(".ui-content").css('height', height);
+    $("#content_about").css('height', height);
 }
 function return_height(){
 	var winDow = $(window).height();
-    $(".ui-content").css('height', winDow);
+    $("#content_home").css('height', winDow);
 }
+$(window).resize(function(){
+		get_window_height();
+		get_about_height();
+		return_height();
+	});
