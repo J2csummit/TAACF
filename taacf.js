2230aad422e412e6279820cecb49ca71b2d8c3c7
J2csummit <J2csummit@mac.com>
2014-08-13 16:17:11 -0400

    5
    Added markup to make code more readable. Added proper content to
    “Schedule Page”.

--- taacf.js
+++ taacf.js
@@ -2,8 +2,11 @@ function get_window_height(){
     // var header = $('#headerpt1').outerHeight(true) + $('#headerpt2').outerHeight(true);
     // var footer = $('#footer').outerHeight(true);
     var winDow = $(window).height();
-    var height = (winDow - 35 - 79 - 36 + 8);
-    $("#content_Schedule").css('height', height);
+    var height1 = (winDow - 35 - 79 - 36 + 8);
+    var height2 = (winDow - 35 - 79);
+    $("#content_Schedule").css('height', height1);
+    $("#content_youth").css('height', height2);
+    $("#content_special").css('height', height2);
 }
 function get_about_height(){
 	var winDow = $(window).height();
